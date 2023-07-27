3f56dc4842496fc242d7e66ff2c3d9a543cc0e59
HanhengHe <EmhRk@outlook.com>
2023-07-26 22:58:07 -0400

    sync funny!

--- prototype/src/main.rs
+++ prototype/src/main.rs
@@ -1,19 +1,21 @@
-use rusty_engine::prelude::*
+use std::{fmt::format, panic::Location};
+
+use rusty_engine::prelude::*;
 
 struct GameState {
-    high_score: u32,
+    // high_score: u32,
     current_score: u32,
-    enemy_labels: Vec<String>,
-    spawn_timer: Timer,
+    ferris_index: i32,
+    // spawn_timer: Timer,
 }
 
 impl Default for GameState {
     fn default() -> Self {
-        Self { 
-            high_score: 0, 
-            current_score: 0, 
-            enemy_labels: Vec::new(), 
-            spawn_timer: Timer::from_seconds(1.0, false) 
+        Self {
+            // high_score: 0,
+            current_score: 0,
+            ferris_index: 0,
+            // spawn_timer: Timer::from_seconds(1.0, false),
         }
     }
 }
@@ -21,12 +23,71 @@ impl Default for GameState {
 fn main() {
     let mut game = Game::new();
 
-    let player = game.add_sprite("player", SpritePreset::RacingCarBlue);
+    let player: &mut Sprite = game.add_sprite("player", SpritePreset::RacingCarBlue);
+
+    player.translation = Vec2::new(0.0, 0.0);
+    player.rotation = SOUTH_WEST;
+    player.scale = 1.0;
+    player.collision = true;
+
+    let car1 = game.add_sprite("car1", SpritePreset::RacingCarYellow);
+    car1.translation = Vec2::new(300.0, 0.0);
+    car1.collision = true;
 
     game.add_logic(game_logic);
     game.run(GameState::default());
 }
 
 fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
-    
+    // collisions
+    for event in engine.collision_events.drain(..) {
+        if event.state == CollisionState::Begin && event.pair.one_starts_with("player") {
+            // remove the sprite the player collided with
+            for label in [event.pair.0, event.pair.1] {
+                if label != "player" {
+                    engine.sprites.remove(&label);
+                }
+            }
+            game_state.current_score += 1;
+        }
+    }
+
+    // player movement
+    let player = engine.sprites.get_mut("player").unwrap();
+    const MOVEMENT_SPEED: f32 = 100.0;
+    if engine
+        .keyboard_state
+        .pressed_any(&[KeyCode::Up, KeyCode::W])
+    {
+        player.translation.y += MOVEMENT_SPEED * engine.delta_f32;
+    }
+    if engine
+        .keyboard_state
+        .pressed_any(&[KeyCode::Down, KeyCode::S])
+    {
+        player.translation.y -= MOVEMENT_SPEED * engine.delta_f32;
+    }
+    if engine
+        .keyboard_state
+        .pressed_any(&[KeyCode::Left, KeyCode::A])
+    {
+        player.translation.x -= MOVEMENT_SPEED * engine.delta_f32;
+    }
+    if engine
+        .keyboard_state
+        .pressed_any(&[KeyCode::Right, KeyCode::D])
+    {
+        player.translation.x += MOVEMENT_SPEED * engine.delta_f32;
+    }
+
+    // mouse input
+    if engine.mouse_state.just_pressed(MouseButton::Left) {
+        if let Some(mouse_location) = engine.mouse_state.location() {
+            let label = format!("ferris{}", game_state.ferris_index);
+            game_state.ferris_index += 1;
+            let ferris = engine.add_sprite(label.clone(), "sprite\\rolling\\block_square.png");
+            ferris.translation = mouse_location;
+            ferris.collision = true;
+        }
+    }
 }
